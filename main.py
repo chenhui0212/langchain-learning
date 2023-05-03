64d4ff718f670dfe3fb215c9e4c2fc022c7c31eb
chenhui <chenhui19900212@gmail.com>
2023-05-03 17:21:54 +0800

    Add more examples

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ from langchain.chains import ConversationChain
 from langchain.chains import SimpleSequentialChain
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
+from langchain.document_loaders import HNLoader
+from langchain.document_loaders import TextLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import FAISS
+from langchain.embeddings import OpenAIEmbeddings
 
 load_dotenv(find_dotenv())
 
@@ -46,6 +51,45 @@ second_chain = LLMChain(llm=llm, prompt=second_prompt, verbose=True)
 
 seq_chain = SimpleSequentialChain( chains=[chain, second_chain], verbose=True)
 seq_chain("colorful socks")
+
+
+# %%
+loader = HNLoader("https://news.ycombinator.com/item?id=34422627")
+data = loader.load()
+print(f"Found {len(data)} comments")
+print(f"Here's a sample:\n\n {data[0].page_content}")
+
+
+# %%
+with open('blog.txt') as f:
+    fc = f.read()
+print(f"You have {len(fc)} characters in your blog")
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=500,
+    chunk_overlap=50,
+)
+text = text_splitter.create_documents([fc])
+print(f"Split into {len(text)} documents\n")
+
+print("Preview:")
+print(text[0].page_content, "\n")
+print(text[1].page_content)
+
+
+# %%
+loader = TextLoader('blog.txt')
+doc = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+texts = text_splitter.split_documents(doc)
+
+embeddings = OpenAIEmbeddings()
+db = FAISS.from_documents(texts, embeddings)
+retriver = db.as_retriever()
+
+docs = retriver.get_relevant_documents("How to use structured tools?")
+print("\n\n".join([x.page_content[:200] for x in docs[:2]]))
 
 
 # %%
